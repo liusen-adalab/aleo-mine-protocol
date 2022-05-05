8bcbd56326930b24ac7299aa182b2dbdf6c1b352
liusen <yjghdsss@163.com>
2022-05-05 16:48:39 +0800

    rename crate

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ use tokio_util::codec::{Decoder, Encoder};
 
 #[allow(clippy::large_enum_variant)]
 #[derive(PartialEq, Eq, Debug)]
-pub enum PoolMessage {
+pub enum Message {
     // as in stratum, with an additional protocol version field
     Authorize(Address<Testnet2>, String, u16),
     AuthorizeResult(bool, Option<String>),
@@ -23,9 +23,7 @@ pub enum PoolMessage {
     Submit(u32, <Testnet2 as Network>::PoSWNonce, PoSWProof<Testnet2>),
     // miners might want to know the stale rate, optionally provide a message
     SubmitResult(bool, Option<String>),
-    ProverNumMinute(u64),
-
-    Exit,
+    ProvePerMinute(u64),
 
     Canary,
 }
@@ -33,7 +31,7 @@ pub enum PoolMessage {
 #[allow(dead_code)]
 static VERSION: u16 = 1;
 
-impl PoolMessage {
+impl Message {
     #[allow(dead_code)]
     pub fn version() -> &'static u16 {
         &VERSION
@@ -41,47 +39,44 @@ impl PoolMessage {
 
     pub fn id(&self) -> u8 {
         match self {
-            PoolMessage::Authorize(..) => 0,
-            PoolMessage::AuthorizeResult(..) => 1,
-            PoolMessage::Notify(..) => 2,
-            PoolMessage::Submit(..) => 3,
-            PoolMessage::SubmitResult(..) => 4,
+            Message::Authorize(..) => 0,
+            Message::AuthorizeResult(..) => 1,
+            Message::Notify(..) => 2,
+            Message::Submit(..) => 3,
+            Message::SubmitResult(..) => 4,
 
-            PoolMessage::Canary => 5,
-            PoolMessage::ProverNumMinute(_) => 7,
-            PoolMessage::Exit => 8,
+            Message::Canary => 5,
+            Message::ProvePerMinute(_) => 6,
         }
     }
 
     pub fn name(&self) -> &'static str {
         match self {
-            PoolMessage::Authorize(..) => "Authorize",
-            PoolMessage::AuthorizeResult(..) => "AuthorizeResult",
-            PoolMessage::Notify(..) => "Notify",
-            PoolMessage::Submit(..) => "Submit",
-            PoolMessage::SubmitResult(..) => "SubmitResult",
-            PoolMessage::Canary => "Canary",
-            PoolMessage::ProverNumMinute(_) => "prover_num_in_minute",
-            PoolMessage::Exit => "exit",
+            Message::Authorize(..) => "Authorize",
+            Message::AuthorizeResult(..) => "AuthorizeResult",
+            Message::Notify(..) => "Notify",
+            Message::Submit(..) => "Submit",
+            Message::SubmitResult(..) => "SubmitResult",
+            Message::Canary => "Canary",
+            Message::ProvePerMinute(_) => "prover_num_in_minute",
         }
     }
 }
 
-impl Encoder<PoolMessage> for PoolMessage {
+impl Encoder<Message> for Message {
     type Error = anyhow::Error;
 
-    fn encode(&mut self, item: PoolMessage, dst: &mut BytesMut) -> Result<(), Self::Error> {
+    fn encode(&mut self, item: Message, dst: &mut BytesMut) -> Result<(), Self::Error> {
         dst.extend_from_slice(&0u32.to_le_bytes());
         let mut writer = dst.writer();
         writer.write_all(&[item.id()])?;
         match item {
-            PoolMessage::Authorize(addr, password, version) => {
+            Message::Authorize(addr, password, version) => {
                 bincode::serialize_into(&mut writer, &addr)?;
                 bincode::serialize_into(&mut writer, &password)?;
                 writer.write_all(&version.to_le_bytes())?;
             }
-            PoolMessage::AuthorizeResult(result, message)
-            | PoolMessage::SubmitResult(result, message) => {
+            Message::AuthorizeResult(result, message) | Message::SubmitResult(result, message) => {
                 writer.write_all(&[match result {
                     true => 1,
                     false => 0,
@@ -93,20 +88,19 @@ impl Encoder<PoolMessage> for PoolMessage {
                     writer.write_all(&[0])?;
                 }
             }
-            PoolMessage::Notify(template, difficulty) => {
+            Message::Notify(template, difficulty) => {
                 template.write_le(&mut writer)?;
                 writer.write_all(&difficulty.to_le_bytes())?;
             }
-            PoolMessage::Submit(height, nonce, proof) => {
+            Message::Submit(height, nonce, proof) => {
                 writer.write_all(&height.to_le_bytes())?;
                 nonce.write_le(&mut writer)?;
                 proof.write_le(&mut writer)?;
             }
-            PoolMessage::Canary => return Err(anyhow!("Use of unsupported message")),
-            PoolMessage::ProverNumMinute(num) => {
+            Message::Canary => return Err(anyhow!("Use of unsupported message")),
+            Message::ProvePerMinute(num) => {
                 writer.write_all(&num.to_le_bytes())?;
             }
-            PoolMessage::Exit => {}
         }
         let msg_len = dst.len() - 4;
         dst[..4].copy_from_slice(&(msg_len as u32).to_le_bytes());
@@ -114,9 +108,9 @@ impl Encoder<PoolMessage> for PoolMessage {
     }
 }
 
-impl Decoder for PoolMessage {
+impl Decoder for Message {
     type Error = anyhow::Error;
-    type Item = PoolMessage;
+    type Item = Message;
 
     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         if src.len() < 4 {
@@ -138,7 +132,7 @@ impl Decoder for PoolMessage {
                 let addr = bincode::deserialize_from(&mut *reader)?;
                 let password = bincode::deserialize_from(&mut *reader)?;
                 let version = reader.read_u16::<LittleEndian>()?;
-                PoolMessage::Authorize(addr, password, version)
+                Message::Authorize(addr, password, version)
             }
             1 => {
                 let result = reader.read_u8()? == 1;
@@ -147,18 +141,18 @@ impl Decoder for PoolMessage {
                 } else {
                     None
                 };
-                PoolMessage::AuthorizeResult(result, message)
+                Message::AuthorizeResult(result, message)
             }
             2 => {
                 let template = BlockTemplate::<Testnet2>::read_le(&mut *reader)?;
                 let difficulty = reader.read_u64::<LittleEndian>()?;
-                PoolMessage::Notify(template, difficulty)
+                Message::Notify(template, difficulty)
             }
             3 => {
                 let height = reader.read_u32::<LittleEndian>()?;
                 let nonce = <Testnet2 as Network>::PoSWNonce::read_le(&mut *reader)?;
                 let proof = PoSWProof::<Testnet2>::read_le(&mut *reader)?;
-                PoolMessage::Submit(height, nonce, proof)
+                Message::Submit(height, nonce, proof)
             }
             4 => {
                 let result = reader.read_u8()? == 1;
@@ -167,13 +161,15 @@ impl Decoder for PoolMessage {
                 } else {
                     None
                 };
-                PoolMessage::SubmitResult(result, message)
+                Message::SubmitResult(result, message)
             }
-            7 => {
+            5 => {
+                return Err(anyhow!("cannot decode canary"));
+            }
+            6 => {
                 let num = reader.read_u64::<LittleEndian>()?;
-                PoolMessage::ProverNumMinute(num)
+                Message::ProvePerMinute(num)
             }
-            8 => PoolMessage::Exit,
             _ => {
                 return Err(anyhow!("Unknown message id: {}", msg_id));
             }
@@ -192,30 +188,17 @@ mod test {
     use bytes::BytesMut;
     use tokio_util::codec::{Decoder, Encoder};
 
-    use crate::PoolMessage;
+    use crate::Message;
 
     #[test]
     fn test_codec() {
-        {
-            let mut canary = PoolMessage::Canary;
-            let msg = PoolMessage::Exit;
+        let mut canary = Message::Canary;
+        let msg = Message::ProvePerMinute(10);
 
-            let mut bytes = BytesMut::new();
-            canary.encode(msg, &mut bytes).unwrap();
-            let decoded = canary.decode(&mut bytes).unwrap().unwrap();
+        let mut bytes = BytesMut::new();
+        canary.encode(msg, &mut bytes).unwrap();
+        let decoded = canary.decode(&mut bytes).unwrap().unwrap();
 
-            assert_eq!(decoded, PoolMessage::Exit);
-        }
-
-        {
-            let mut canary = PoolMessage::Canary;
-            let msg = PoolMessage::ProverNumMinute(10);
-
-            let mut bytes = BytesMut::new();
-            canary.encode(msg, &mut bytes).unwrap();
-            let decoded = canary.decode(&mut bytes).unwrap().unwrap();
-
-            assert_eq!(decoded, PoolMessage::ProverNumMinute(10));
-        }
+        assert_eq!(decoded, Message::ProvePerMinute(10));
     }
 }
